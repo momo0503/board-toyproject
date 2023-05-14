1f9191b9e060cd17d0a8becbe916b4e0a644c029
momonote2 <yiyi6464@naver.com>
2023-05-14 14:46:26 +0900

    #5- 스프링 부트 프로젝트 시작
    *스프링 웹
    *액츄에이터
    *롬북
    * DevTools

---
+++ src/main/java/com/toyboard/boardtoyproject/BoardToyprojectApplication.java
@@ -0,0 +1,13 @@
+package com.toyboard.boardtoyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardToyprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardToyprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toyboard/boardtoyproject/BoardToyprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyboard.boardtoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardToyprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
