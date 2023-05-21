50eff59e279e98b07d01155a9b8c2daad85b579a
momonote2 <yiyi6464@naver.com>
2023-05-21 17:36:16 +0900

    #8 - 도메인 클래스 설계 (게시글, 댓글 ) 도메인 정보를 설계해보았다. 기초적인 게시글과 댓글 표현이 가능하도록 함. DB와의 연동을 위한 추가적인 표현은 추후에 업데이트 될 수 있음.

--- src/main/java/com/toyboard/boardtoyproject/BoardToyprojectApplication.java
+++ src/main/java/com/toyboard/boardtoyproject/BoardToyprojectApplication.java
@@ -8,6 +8,9 @@ public class BoardToyprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BoardToyprojectApplication.class, args);
+
+		System.out.println("hi");
 	}
+
 
 }
